dae8a963fee1983dda2bc82fcada638a5e053a13
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-10-31 16:43:37 +0200

    docs: secret key usage

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 http://url/search/search/id/<id>- Search by id (returns 1 cocktail)
 http://url/search/search/ids/<ids (1,2,3)>- Search by multiple ids (returns all found cocktails)
 http://url/search/name/<name> - Search by name.
+
+If SECRET_KEY is defined in .env file, then add ?key=<key> to your url.
--- index.js
+++ index.js
@@ -39,7 +39,7 @@ mongoose.connection.on("disconnected", () => {
 connect();
 
 function checkSecret(req, res, next) {
-  if (req.query.key === process.env.SECRET_KEY) {
+  if (req.query.key === process.env.SECRET_KEY || undefined) {
     next();
   } else {
     res.status(403).json({ message: "Access denied. Invalid key." });
