db7dfcfc159449cee99d074aa8da71b3375871eb
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-10-31 17:14:23 +0200

    refactor: removed unused routes

--- routes/search.js
+++ routes/search.js
@@ -9,62 +9,6 @@ const { param, validationResult } = require("express-validator");
 const MAX_IDS_PER_QUERY = process.env.MAX_IDS_PER_QUERY || 100;
 const MIN_CHAR_PER_NAME_SEARCH = process.env.MIN_CHAR_PER_NAME_SEARCH || 3;
 
-router.get(
-  "/id/:id",
-  [
-    // Use express-validator to sanitize the id parameter
-    param("id").trim().escape(),
-  ],
-  async (req, res) => {
-    try {
-      const targetId = req.params.id;
-      const drink = await Drink.findOne({ id: targetId }).exec();
-
-      if (!drink) {
-        console.log("No drink found with id:", targetId);
-        return res.status(404).json({ message: "Drink not found" });
-      }
-
-      return res.status(200).json(drink);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    }
-  }
-);
-
-router.get(
-  "/ids/:ids",
-  [
-    param("ids").isArray().withMessage("ids should be an array"),
-    param("ids.*").trim().escape(),
-  ],
-  async (req, res) => {
-    try {
-      const targetIds = req.params.ids.split(",");
-
-      if (targetIds.length > MAX_IDS_PER_QUERY) {
-        console.log(`targetIds length is more than ${MAX_IDS_PER_QUERY}`);
-        return res.status(400).json({
-          message: `Cannot search for more than ${MAX_IDS_PER_QUERY} ids.`,
-        });
-      }
-
-      const drinks = await Drink.find({ id: { $in: targetIds } }).exec();
-
-      if (!drinks || drinks.length == 0) {
-        console.log("No drink found with id:", targetIds);
-        return res.status(404).json({ message: "Drinks not found" });
-      }
-
-      return res.status(200).json(drinks);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    }
-  }
-);
-
 router.get("/name/:name", [param("name").trim().escape()], async (req, res) => {
   try {
     const targetName = req.params.name;
