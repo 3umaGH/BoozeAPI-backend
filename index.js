529a89d3f0a679450101e6e1d597b7957e721a53
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-10-31 14:03:40 +0200

    feat: Search route

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const port = 3001
 app.use('/assets', express.static('assets'));
 app.use(cors());
 
+
+
+
 app.get('/api', (req, res) => {
   res.send(`Hello World from Express backend!`)
 })
 
-app.listen(port, () => {
-  console.log(`CocktailDB Api is running on ${port} port.`)
-})
+const searchRouter = require("./routes/search");
+app.use("/search",searchRouter);
+
+
+
+app.listen(port)
