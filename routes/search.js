19af35a20752ee6fd49252d54acdd1212077c13f
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-10-31 15:47:09 +0200

    feat: search multiple cocktails by id

--- routes/search.js
+++ routes/search.js
@@ -28,6 +28,38 @@ router.get(
   }
 );
 
+router.get(
+  "/ids/:ids",
+  [
+    param("ids").isArray().withMessage("ids should be an array"),
+    param("ids.*").trim().escape(),
+  ],
+  async (req, res) => {
+    try {
+      const targetIds = req.params.ids.split(",");
+
+      if (targetIds.length > 100) {
+        console.log("targetIds length is more than 100");
+        return res
+          .status(404)
+          .json({ message: "Cannot search for more than 100 ids." });
+      }
+
+      const drinks = await Drink.find({ id: { $in: targetIds } }).exec();
+
+      if (!drinks || drinks.length == 0) {
+        console.log("No drink found with id:", targetIds);
+        return res.status(404).json({ message: "Drinks not found" });
+      }
+
+      return res.status(200).json(drinks);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
 router.get("*", (req, res) => {
   res.status(404).json({ message: "Not found" });
 });
