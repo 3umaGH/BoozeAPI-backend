e1c5c9f0f52cc0764d0e2691b537477a646634c2
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-10-31 17:29:27 +0200

    feat: look up random cocktails

---
+++ routes/lookup.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const Drink = require("../models/drink");
+
+require("dotenv").config();
+
+const { query } = require("express-validator");
+
+const MAX_RANDOM_COCKTAILS_PER_REQUEST = process.env.MAX_RANDOM_COCKTAILS_PER_REQUEST || 50;
+
+router.get(
+  "/random",
+  [
+    query("amount").trim().escape(),
+  ],
+  async (req, res) => {
+    try {
+      let amount = req.query.amount || 10;
+
+      if (isNaN(amount)) {
+        return res.status(400).json({ message: "Amount must be a number" });
+      } else amount = parseFloat(amount);
+
+      if(amount > MAX_RANDOM_COCKTAILS_PER_REQUEST)
+        return res.status(400).json({ message: `Maximum ${MAX_RANDOM_COCKTAILS_PER_REQUEST} cocktails per request.` });
+      
+
+      const randomCocktails = await Drink.aggregate([
+        { $sample: { size: amount } },
+      ]).exec();
+
+      if (!randomCocktails || randomCocktails.length === 0) {
+        console.log("Unable to fetch random drinks", targetId);
+        return res.status(404).json({ message: "Unable to get random drinks" });
+      }
+
+      return res.status(200).json(randomCocktails);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+module.exports = router;
