16aa0370c5c2c8e81441dcee312504de8e3f17c7
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-01 12:08:01 +0200

    debug: debugging vercel

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(cors());
 
-/*app.use("/drinks", express.static("static/drinks"));*/
+app.use("/", express.static("public"));
+
 
 // DB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
